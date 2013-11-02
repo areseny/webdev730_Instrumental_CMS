55939dacd39a2ce1d421a5fc4cb0161bf429e830
WebDevStar730 <webdevstarjus730@gmail.com>
2013-11-02 06:06:17 -0200

    Create Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstrumentalSescBrasil::Application.config.secret_key_base = 'd06655cdef844ff2d00c3624bcec02f8ef5282449598a38b0dcbdb102edeca5a52281ea3e6703240247ce07e27f146a0b9195c1936f5e265868cdbd50b97702e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InstrumentalSescBrasil::Application.config.session_store :cookie_store, key: '_instrumental_sesc_brasil_session'
