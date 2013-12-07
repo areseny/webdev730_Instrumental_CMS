c97227b089ed16e05d0918f68f7842455fc9fe8a
WebDevStar730 <webdevstarjus730@gmail.com>
2013-12-07 19:48:38 -0200

    Display error messages on Ajax requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def process_error(error)
+    if request.xhr?
+      flash.now[:error] = "server_error"
+      render :error, format: :js
+      if Rails.env.development?
+        logger.error error
+        logger.error error.backtrace.join("\n")
+      else
+        # TODO: Notify Airbrake
+      end
+    else
+      raise error
+    end
+  end
+  rescue_from ::Exception, with: :process_error
+
 end
