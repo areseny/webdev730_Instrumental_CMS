a6c1c7c4b84dbdbddc431e886d617f34441da522
WebDevStar730 <webdevstarjus730@gmail.com>
2013-12-07 18:40:35 -0200

    Add 'about us' page

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,25 @@
+module HomeHelper
+
+  def about_us_feature_image
+    title = t("home.about_us.title")
+    image_tag("aboutus-feature.jpg",
+              size: '900x600', class: 'round', alt: title)
+  end
+
+  def about_us_sescsp_link
+    title = t('links.sescsp.title')
+    url = t('links.sescsp.url')
+    image = image_tag("aboutus-logo-sescsp.jpg", size: '290x130',
+                      class: 'round', alt: 'title')
+    link_to image, url, target: '_blank', title: title
+  end
+
+  def about_us_sesctv_link
+    title = t('links.sesctv.title')
+    url = t('links.sesctv.url')
+    image = image_tag("aboutus-logo-sesctv.jpg", size: '290x130',
+                      class: 'round', alt: 'title')
+    link_to image, url, target: '_blank', title: title
+  end
+
+end
