9aa40f4cf831d6be0a2c0c0e59c4a1e811e46d36
WebDevStar730 <webdevstarjus730@gmail.com>
2014-01-15 23:04:55 -0200

    Load env variables from dotenv in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 InstrumentalSescBrasil::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Load env variables from .env
+  require 'dotenv'
+  Dotenv.load
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
