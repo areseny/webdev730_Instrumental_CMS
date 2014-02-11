07d5f1842e257d065ae58861c27ec54ccad7449e
WebDevStar730 <webdevstarjus730@gmail.com>
2014-02-11 20:13:52 -0200

    Upgrade FontAwesome to 4.0.3

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -3,10 +3,10 @@ module AssetsHelper
   def fontawesome_asset_tags
     if Rails.env.production?
       # Use Twitter's CDN server on production
-      raw %(<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">)
+      raw %(<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">)
     else
       # Use local file from /vendor/assets in development
-      raw %(<link href="/assets/font-awesome-3.2.1.css?body=1" media="screen" rel="stylesheet" />)
+      raw %(<link href="/assets/font-awesome-4.0.3.css?body=1" media="screen" rel="stylesheet" />)
     end
   end
 
