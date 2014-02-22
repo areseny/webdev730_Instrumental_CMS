235a0290cb0c72634a82dfbba40d910666977294
WebDevStar730 <webdevstarjus730@gmail.com>
2014-02-22 03:44:28 -0300

    I mean, now

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -3,7 +3,7 @@ module VideoHelper
   def video_player(event)
     if event.playlist?
       ordered_songs = event.songs.ordered.to_a
-      playlist = ordered_songs.map{ |s| s.video.youtube_id }.reverse
+      playlist = ordered_songs.map{ |s| s.video.youtube_id }
       members = ordered_songs.map{ |s| band_members_as_sentence(s.band_members) }
       video_id = params[:videoId].in?(playlist) ? params[:videoId] : playlist.first
       data = {
