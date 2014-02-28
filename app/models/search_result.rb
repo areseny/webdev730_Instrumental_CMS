26c8308bf10a9de3466f2d741d046cfaa374644a
WebDevStar730 <webdevstarjus730@gmail.com>
2014-02-28 02:37:50 -0300

    fix search to use plain_tsquery

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -8,8 +8,8 @@ class SearchResult < ActiveRecord::Base
   scope :more, -> { where(result_type: "More") }
 
   scope :search, -> (query) {
-    where(%[(((setweight(to_tsvector('portuguese', f_unaccent(coalesce("search_results"."title"::text, ''))), 'A') || setweight(to_tsvector('portuguese', f_unaccent(coalesce("search_results"."content"::text, ''))), 'B')) @@ (to_tsquery('portuguese', ''' ' || f_unaccent(?) || ' '''))))], query)
-    .order(%[((ts_rank((setweight(to_tsvector('portuguese', f_unaccent(coalesce("search_results"."title"::text, ''))), 'A') || setweight(to_tsvector('portuguese', f_unaccent(coalesce("search_results"."content"::text, ''))), 'B')), (to_tsquery('portuguese', ''' ' || f_unaccent(#{sanitize(query)}) || ' ''')), 0))) DESC])
+    where(%[(((setweight(to_tsvector('portuguese', f_unaccent(coalesce("search_results"."title"::text, ''))), 'A') || setweight(to_tsvector('portuguese', f_unaccent(coalesce("search_results"."content"::text, ''))), 'B')) @@ (plainto_tsquery('portuguese', f_unaccent(:query)))))], :query => query)
+    .order(%[((ts_rank((setweight(to_tsvector('portuguese', f_unaccent(coalesce("search_results"."title"::text, ''))), 'A') || setweight(to_tsvector('portuguese', f_unaccent(coalesce("search_results"."content"::text, ''))), 'B')), (plainto_tsquery('portuguese', f_unaccent(#{sanitize(query)}))), 0))) DESC])
   }
 
   def self.genres_list
