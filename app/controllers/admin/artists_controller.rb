b5c5b0c6cdecb022535b04a5ddbcea5a1a314f61
WebDevStar730 <webdevstarjus730@gmail.com>
2015-01-20 23:08:56 -0200

    Edit playlist embed in artist.

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -1,5 +1,4 @@
 class Admin::ArtistsController < AdminController
-
   def index
   end
 
@@ -17,6 +16,7 @@ class Admin::ArtistsController < AdminController
 
   def create
     @artist = Artist.new(artist_params)
+
     if @artist.save
       invalidate_cache
       flash[:success] = "admin.artists.create"
@@ -32,6 +32,7 @@ class Admin::ArtistsController < AdminController
 
   def update
     @artist = Artist.find_by_slug!(params[:id])
+
     if @artist.update_attributes(artist_params)
       flash[:success] = "admin.artists.update"
       redirect_to admin_artists_path
@@ -43,6 +44,7 @@ class Admin::ArtistsController < AdminController
   def destroy
     @artist = Artist.find_by_slug!(params[:id])
     @artist.destroy!
+
     flash[:success] = "admin.artists.destroy"
     redirect_to admin_artists_path
   end
@@ -52,12 +54,11 @@ class Admin::ArtistsController < AdminController
   def artist_params
     params.require(:artist)
           .permit(:name, :description, :instrument_names, :genre_names,
-                  :facebook_page, :twitter_widget_id,
+                  :facebook_page, :twitter_widget_id, :playlist_embed,
                   :banner, :thumbnail)
   end
 
   def invalidate_cache
     expire_fragment("artists-list")
   end
-
 end
